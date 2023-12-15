dd047f2b4e8d21a203bd06ae1715fd29635935f3
sluo <41530060+lu0x1a0@users.noreply.github.com>
2023-12-16 09:35:26 +1100

    add enum

--- datacron/yahoo-finance/source.py
+++ datacron/yahoo-finance/source.py
@@ -1,16 +1,35 @@
 import yfinance as yf
 import yaml
 
+import enum
+
+
+class PERIOD(enum.Enum):
+    d1 = "1d"
+    d5 = "5d"
+    m1 = "1mo"
+    m3 = "3mo"
+    m6 = "6mo"
+    y1 = "1y"
+    y2 = "2y"
+    y5 = "5y"
+    y10 = "10y"
+    ytd = "ytd"
+    max = "max"
+
 
 class source:
     def __init__(self, symbol: str):
         self.ticker = yf.Ticker(symbol)
 
     def getLatestDayData(self):
-        return self.tickers()
+        return self.ticker.history(period="1d", interval="1m")
 
-    def getDataByDay(self):
-        return self.tickers()
+    def getDataByPeriod(self, period: PERIOD):
+        return self.ticker.history(period=period, interval="1m")
+
+    def getDataByDate(self, start_date, end_date):
+        return self.ticker.history(start=start_date, end=end_date, interval="1m")
 
 
 if __name__ == "__main__":
