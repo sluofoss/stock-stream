fb9e55a612466292efb04fc2414277d2421f970b
sluo <41530060+lu0x1a0@users.noreply.github.com>
2023-12-25 22:52:06 +1100

    make class source obsolete

--- datacron/yahoo-finance/awslambda.py
+++ datacron/yahoo-finance/awslambda.py
@@ -6,26 +6,23 @@ from source import Source
 import logging.config
 import yaml
 import sys
+import yfinance as yf
 
 logger = logging.getLogger("lambda")
 logger.info("awslambda.PY is here!!!!")
 logger = logging.getLogger("lambda")
-with open('logconfig.yaml', 'r') as configfile:
+with open("logconfig.yaml", "r") as configfile:
     configdict = yaml.safe_load(configfile)
     logging.config.dictConfig(configdict)
 
-#logger.setLevel(logging.DEBUG)
-#h = logging.FileHandler('./lambda.log')
-#h.setLevel(logging.DEBUG)
-#logger.addHandler(h)
-#logger.info("mock started")
+# logger.setLevel(logging.DEBUG)
+# h = logging.FileHandler('./lambda.log')
+# h.setLevel(logging.DEBUG)
+# logger.addHandler(h)
+# logger.info("mock started")
 
-#logging.getLogger('yfinance').addHandler(logging.FileHandler('./lambda.log'))
-#logger.propagate = True
-
-def check_symbol_info(symbol):
-    s = Source(symbol)
-    return s.ticker.info
+# logging.getLogger('yfinance').addHandler(logging.FileHandler('./lambda.log'))
+# logger.propagate = True
 
 
 def lambda_get_symbols_data_multi(event, context):
@@ -38,20 +35,26 @@ def lambda_get_symbols_data_multi(event, context):
     """
     logger.info(event)
     logger.info(context)
-    
+
     import pandas as pd
 
     df = pd.read_csv("./ASX_Listed_Companies_17-12-2023_01-39-05_AEDT.csv")
     # print(df)
     symbols = [x + ".AX" for x in df["ASX code"]]
 
+    start_date = event["time"]  # TODO: Confirm this
+    next_day = datetime.datetime.strptime(start_date, "%Y-%m-%d") + datetime.timedelta(
+        days=1
+    )
+
     get_symbols_data_multi(
         symbols,
         max_worker=50,
         s3_save_bucket=os.getenv("S3_STORE_BUCKET"),
-        local_save = True, # TODO: remove this 
-        exec_date=event["time"],  # TODO: Confirm this
+        local_save=True,  # TODO: remove this
+        yf_hist_arg={"start": start_date, "end": next_day, "interval": "1m"},
     )
+
 
 def lambda_check_symbols_info_multi(event, context):
     """check symbol info
@@ -63,18 +66,14 @@ def lambda_check_symbols_info_multi(event, context):
     """
     logger.info(event)
     logger.info(context)
-    
+
     import pandas as pd
 
     df = pd.read_csv("./ASX_Listed_Companies_17-12-2023_01-39-05_AEDT.csv")
     # print(df)
     symbols = [x + ".AX" for x in df["ASX code"]]
 
-    check_symbols_info_multi(
-        symbols,
-        max_worker=50, 
-        print_data=True
-    )
+    check_symbols_info_multi(symbols, max_worker=50, print_data=True)
 
 
 def get_symbols_data_multi(
@@ -83,63 +82,59 @@ def get_symbols_data_multi(
     print_data=False,
     local_save: str = None,
     s3_save_bucket: str = None,
-    exec_date=None,
+    s3_parent_key="yfinance/min",
+    yf_hist_args: dict = {"interval": "1m"},
 ):
     with concurrent.futures.ThreadPoolExecutor(max_workers=max_worker) as executor:
-        if exec_date is None:
-            future_to_symbol = {
-                executor.submit(Source(symbol).getLatestDayData): symbol
-                for symbol in symbols
-            }
-            exec_date = datetime.date.today()
-            logger.info("Today's date:", exec_date)
-        else:
-            next_day = datetime.datetime.strptime(
-                exec_date, "%Y-%m-%d"
-            ) + datetime.timedelta(days=1)
-            future_to_symbol = {
-                executor.submit(
-                    Source(symbol).getDataByDate, exec_date, next_day
-                ): symbol
-                for symbol in symbols
-            }
-            logger.info(f"Get Data on Date {exec_date} before {next_day}")
+        logger.info(f"yfinance receiving arguments: {yf_hist_args}")
+
+        future_to_symbol = {
+            executor.submit(
+                lambda kwargs: yf.Ticker(symbol).history(**kwargs), yf_hist_args
+            ): symbol
+            for symbol in symbols
+        }
 
         logger.info("created future")
+
         for future in future_to_symbol:
             symbol = future_to_symbol[future]
             # print("symbol format is weird: ", symbol, flush=True)
             try:
                 data = future.result()
             except Exception as exc:
-                logger.error(f"{symbol} generated an exception: {exc}" )
+                logger.error(f"{symbol} generated an exception: {exc}")
             else:
                 logger.info(
                     f"{symbol} Symbol request is successful with len {len(data)}"
                 )
                 if print_data:
-                    logger.info(f'{symbol}: {data}')
+                    logger.info(f"{symbol}: {data}")
                 if local_save:
-                    logger.info(f'{symbol}:Saving to local')
-                    if not os.path.exists(f"./mocks3yfinance/{symbol}/"):       
-                        os.makedirs(f"./mocks3yfinance/{symbol}/") 
+                    logger.info(f"{symbol}:Saving to local")
+                    if not os.path.exists(f"./mocks3yfinance/{symbol}/"):
+                        os.makedirs(f"./mocks3yfinance/{symbol}/")
                     data.to_parquet(
-                        #f"./mocks3yfinance/{symbol}/{exec_date}.parquet.gzip"
-                        f"./mocks3yfinance/{symbol}/{exec_date}.parquet",
+                        # f"./mocks3yfinance/{symbol}/{exec_date}.parquet.gzip"
+                        f"./mocks3yfinance/{symbol}/{yf_hist_args.get('start',datetime.date.today())}.parquet",
                         compression="gzip",
                     )
                 if s3_save_bucket:
-                    logger.info(f'{symbol}: Saving to s3')
+                    logger.info(f"{symbol}: Saving to s3")
                     data.to_parquet(
-                        #f"./mocks3yfinance/{symbol}/{exec_date}.parquet.gzip"
-                        f"s3://{s3_save_bucket}/yfinance/min/{symbol}/{exec_date}.parquet",
+                        # f"./mocks3yfinance/{symbol}/{exec_date}.parquet.gzip"
+                        f"s3://{s3_save_bucket}/{s3_parent_key}/{symbol}/{yf_hist_args.get('start',datetime.date.today())}.parquet",
                         compression="gzip",
                     )
+
 
 def check_symbols_info_multi(symbols, max_worker=10, print_data=False):
     with concurrent.futures.ThreadPoolExecutor(max_workers=max_worker) as executor:
         future_to_symbol = {
-            executor.submit(check_symbol_info, symbol): symbol for symbol in symbols
+            executor.submit(
+                lambda symbol: yf.Ticker(symbol).ticker.info, symbol
+            ): symbol
+            for symbol in symbols
         }
         logger.info("created future")
         for future in future_to_symbol:
@@ -147,7 +142,7 @@ def check_symbols_info_multi(symbols, max_worker=10, print_data=False):
             try:
                 data = future.result()
             except Exception as exc:
-                logger.error(f"{symbol} generated an exception: {exc}" )
+                logger.error(f"{symbol} generated an exception: {exc}")
             else:
                 logger.info(
                     f"{symbol} Symbol request is successful with len {len(data)}"
