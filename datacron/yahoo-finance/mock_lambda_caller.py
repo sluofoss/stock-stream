35d0ae2ae5cfda163e6a267fb1219727098cbb84
sluo <41530060+lu0x1a0@users.noreply.github.com>
2023-12-26 00:02:15 +1100

    add support for arb env file input

--- datacron/yahoo-finance/mock_lambda_caller.py
+++ datacron/yahoo-finance/mock_lambda_caller.py
@@ -2,10 +2,20 @@ import dotenv
 
 from awslambda import lambda_get_symbols_data_multi, lambda_check_symbols_info_multi
 
-dotenv.load_dotenv()
+import argparse
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Read a string argument.")
 
-event = {'time':'2023-12-14'}
-context = None
-lambda_get_symbols_data_multi(event, context)
-#lambda_check_symbols_info_multi(event, context)
+    parser.add_argument("-e", "--env", type=str, help="env file", required=True)
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    if not dotenv.load_dotenv(args.env):
+        raise Exception("environment file empty or not exist")
+
+    event = {"time": "2023-12-14"}
+    context = None
+    lambda_get_symbols_data_multi(event, context)
+    # lambda_check_symbols_info_multi(event, context)
