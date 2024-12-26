672d505bc3c47c53e03624a6478b0be31f2de2f1
sluo <41530060+sluofoss@users.noreply.github.com>
2024-12-26 19:12:09 +1100

    fix path lookup of lambda dir

--- tf-iaac/yahoo-finance-cron/modules/pipeline/locals.tf
+++ tf-iaac/yahoo-finance-cron/modules/pipeline/locals.tf
@@ -1,6 +1,6 @@
 locals {
   #description   = "the folder which the data cron job code reside, the package should also be zipped there."
   #type          = string
-  datacron_yfinance_folder       = "${path.module}/../../../datacron/yahoo-finance"
+  datacron_yfinance_folder       = "${path.module}/../../../../datacron/yahoo-finance"
     # pipeline //modules//yahoo-finance//datacron//tf-iaac//stock_2023
 }
