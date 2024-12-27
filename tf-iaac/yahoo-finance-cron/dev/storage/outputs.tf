2324f559dde05510c57b60a0950f3e4a8166bd53
sluo <41530060+sluofoss@users.noreply.github.com>
2024-12-28 03:17:10 +1100

    redeploy successful

--- tf-iaac/yahoo-finance-cron/dev/storage/outputs.tf
+++ tf-iaac/yahoo-finance-cron/dev/storage/outputs.tf
@@ -2,3 +2,8 @@ output "s3_bucket_arn" {
   value       = module.storage.s3_bucket_arn
   description = "The ARN of the S3 bucket"
 }
+
+output "s3_bucket_id" {
+  value       = module.storage.s3_bucket_id
+  description = "The ID/Name of the S3 bucket"
+}
--- tf-iaac/yahoo-finance-cron/modules/storage/outputs.tf
+++ tf-iaac/yahoo-finance-cron/modules/storage/outputs.tf
@@ -2,3 +2,8 @@ output "s3_bucket_arn" {
   value       = aws_s3_bucket.project_storage.arn
   description = "The ARN of the S3 bucket"
 }
+
+output "s3_bucket_id" {
+  value       = aws_s3_bucket.project_storage.id
+  description = "The ID/Name of the S3 bucket"
+}
