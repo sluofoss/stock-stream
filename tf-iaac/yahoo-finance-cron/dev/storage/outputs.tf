40f62551d3b978333491c562ebcb6fed54c8f820
sluo <41530060+sluofoss@users.noreply.github.com>
2025-01-04 04:38:58 +1100

    add ecr repo

--- tf-iaac/yahoo-finance-cron/dev/storage/outputs.tf
+++ tf-iaac/yahoo-finance-cron/dev/storage/outputs.tf
@@ -7,3 +7,22 @@ output "s3_bucket_id" {
   value       = module.storage.s3_bucket_id
   description = "The ID/Name of the S3 bucket"
 }
+output "ecr_repository_arn" {
+  value       = module.storage.ecr_repository_arn
+  description = "The ARN of the ECR Repo"
+}
+
+output "ecr_repository_name" {
+  value       = module.storage.ecr_repository_name
+  description = "The Name of the ECR Repo"
+}
+
+output "ecr_repository_registry_id" {
+  value       = module.storage.ecr_repository_registry_id
+  description = "The registry id of the ECR Repo"
+}
+
+output "ecr_repository_url" {
+  value       = module.storage.ecr_repository_url
+  description = "The URL of the ECR Repo"
+}
