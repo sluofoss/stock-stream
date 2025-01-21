482dfad8bfc30ee94a0a874eae4c2d3bc3013138
sluo <41530060+sluofoss@users.noreply.github.com>
2025-01-21 18:29:03 +1100

    push image to ecr

---
+++ datacron/yahoo-finance/deploy_image.sh
@@ -0,0 +1,15 @@
+AWS_ACCCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
+
+AWS_REGION=$(aws configure get region)
+
+echo $AWS_ACCCOUNT_ID 
+echo $AWS_REGION
+
+aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
+
+#TODO parametrize the dev env
+ecr_repository_url=$(cd tf-iaac/yahoo-finance-cron/dev/storage; terraform output ecr_repository_url | tr -d '"')
+echo $ecr_repository_url
+
+docker tag docker-image:test $ecr_repository_url:latest
+time docker push $ecr_repository_url:latest
